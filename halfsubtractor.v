ef92784d85864c125a19b44539f2108c95bc6300
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-25 03:40:23 -0700

    Create halfsubtractor.v

---
+++ halfsubtractor.v
@@ -0,0 +1,43 @@
+module halfsub(
+    input a,b,
+    output d,bo
+    );
+assign d=a^b;
+assign bo=(~a)&b;
+endmodule
+
+Test bench:
+module halfsubtest;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire d;
+	wire bo;
+
+	// Instantiate the Unit Under Test (UUT)
+	halfsub uut (
+		.a(a), 
+		.b(b), 
+		.d(d), 
+		.bo(bo)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=1'b0;b=1'b0;
+      #100;a=1'b0;b=1'b1;
+      #100;a=1'b1;b=1'b0;
+      #100;a=1'b1;b=1'b1;		
+		// Add stimulus here
+
+	end
+      
+endmodule
+
