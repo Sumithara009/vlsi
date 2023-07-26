8620928e6fa64325a165ce030dcb1ef4a5c2caad
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:11:26 -0700

    Update fullsubtractor.v

--- fullsubtractor.v
+++ fullsubtractor.v
@@ -1,3 +1,4 @@
+//20L152-SUMITHA R A
 module fullsub(
     input a,b,c,
     output d,bo
@@ -10,47 +11,5 @@ xor x2(x[3],x[0],c);
 not n2(x[4],x[0]);
 and a2(x[5],x[4],c);
 or o1(bo,x[5],x[2]);
-endmodule
-
-Testbench:
-
-module fullsubtest;
-
-	// Inputs
-	reg a;
-	reg b;
-	reg c;
-
-	// Outputs
-	wire d;
-	wire bo;
-
-	// Instantiate the Unit Under Test (UUT)
-	fullsub uut (
-		.a(a), 
-		.b(b), 
-		.c(c), 
-		.d(d), 
-		.bo(bo)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-		c = 0;
-
-		// Wait 100 ns for global reset to finish
-		a=1'b0;b=1'b0;c=1'b0;
-		#100 a=1'b0;b=1'b0;c=1'b1;
-		#100 a=1'b0;b=1'b1;c=1'b0;
-		#100 a=1'b0;b=1'b1;c=1'b1;
-		#100 a=1'b1;b=1'b0;c=1'b0;
-		#100 a=1'b1;b=1'b0;c=1'b1;
-		#100 a=1'b1;b=1'b1;c=1'b0;
-		#100 a=1'b1;b=1'b1;c=1'b1;
-	
-	end
-      
 endmodule
 
