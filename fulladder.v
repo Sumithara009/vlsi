f76b20000f21bd91639061ec496037a880b4a3e0
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:07:40 -0700

    Update fulladder.v

--- fulladder.v
+++ fulladder.v
@@ -1,4 +1,4 @@
-Program:   //20L152-SUMITHA R A
+//20L152-SUMITHA R A
 
 module fulladd(
     input a,b,cin,
@@ -12,47 +12,5 @@ and g4(x[2],x[0],x[1]);
 or g5(co,x[2],x[1]);
 endmodule
 
-Test bench:
 
-module fulladdtest;
-
-	// Inputs
-	reg a;
-	reg b;
-	reg cin;
-
-	// Outputs
-	wire s;
-	wire co;
-
-	// Instantiate the Unit Under Test (UUT)
-	fulladd uut (
-		.a(a), 
-		.b(b), 
-		.cin(cin), 
-		.s(s), 
-		.co(co)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-		cin = 0;
-
-		// Wait 100 ns for global reset to finish
-		#100;a=1'b0;b=1'b0;cin=1'b0;
-		#100;a=1'b0;b=1'b0;cin=1'b1;
-		#100;a=1'b0;b=1'b1;cin=1'b0;
-		#100;a=1'b0;b=1'b1;cin=1'b1;
-		#100;a=1'b1;b=1'b0;cin=1'b0;
-		#100;a=1'b1;b=1'b0;cin=1'b1;
-		#100;a=1'b1;b=1'b1;cin=1'b0;
-		#100;a=1'b1;b=1'b1;cin=1'b1;
-        
-		// Add stimulus here
-
-	end
-      
-endmodule
 
