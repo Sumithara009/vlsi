38bcbc5dba3ddfe1ab83283fa2d09826a5a2b0b0
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:04:13 -0700

    Update halfadder.v

--- halfadder.v
+++ halfadder.v
@@ -1,4 +1,4 @@
-Program:    //20L152 SUMITHA R A
+//20L152 SUMITHA R A
 
 module halfadd(
     input a,b,
@@ -7,42 +7,6 @@ module halfadd(
 assign s=a^b;
 assign co=a&b;
 
-endmodule
-
-Testbench:
-
-module halfaddtest;
-
-	// Inputs
-	reg a;
-	reg b;
-
-	// Outputs
-	wire s;
-	wire co;
-
-	// Instantiate the Unit Under Test (UUT)
-	halfadd uut (
-		.a(a), 
-		.b(b), 
-		.s(s), 
-		.co(co)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-
-		// Wait 100 ns for global reset to finish
-		#100;a=1'b0;b=1'b0;
-                #100;a=1'b0;b=1'b1;
-		#100;a=1'b1;b=1'b0;
-		#100;a=1'b1;b=1'b1;
-		// Add stimulus here
-
-	end
-      
 endmodule
 
 
