2bb77663ed9ee56882024a9cf0350b82a6d87408
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:17:01 -0700

    Update Four_bit_binary_sub.v

--- Four_bit_binary_sub.v
+++ Four_bit_binary_sub.v
@@ -1,4 +1,4 @@
-PROGRAM: //20L152-SUMITHA R A
+//20L152-SUMITHA R A
 
 module binsub(a,b,s,cout);
 input [3:0]a;
@@ -24,43 +24,6 @@ and g4(x[2],x[0],x[1]);
 or g5(co,x[2],x[1]);
 endmodule
 
-Test bench:
 
-module binsubtest;
 
-	// Inputs
-	reg [3:0] a;
-	reg [3:0] b;
-
-	// Outputs
-	wire [3:0] s;
-	wire cout;
-
-	// Instantiate the Unit Under Test (UUT)
-	binsub uut (
-		.a(a), 
-		.b(b), 
-		.s(s), 
-		.cout(cout)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-
-		// Wait 100 ns for global reset to finish
-      #100 a=4'b0001;b=4'b0011;
-      #100 a=4'b0101;b=4'b1011;
-      #100 a=4'b1101;b=4'b0011;
-      #100 a=4'b0011;b=4'b0011;
-      #100 a=4'b1001;b=4'b0011;
-      #100 a=4'b0001;b=4'b1011;
-      #100 a=4'b1111;b=4'b1101;
-        
-		// Add stimulus here
-
-	end
-      
-endmodule
 
