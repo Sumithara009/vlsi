61d6d8916a60ca3408073aa210e99ecd3a879f3e
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:05:28 -0700

    Create halfaddertest.v

---
+++ halfaddertest.v
@@ -0,0 +1,35 @@
+//20L152-SUMITHA R A
+module halfaddtest;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire s;
+	wire co;
+
+	// Instantiate the Unit Under Test (UUT)
+	halfadd uut (
+		.a(a), 
+		.b(b), 
+		.s(s), 
+		.co(co)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=1'b0;b=1'b0;
+    #100;a=1'b0;b=1'b1;
+		#100;a=1'b1;b=1'b0;
+		#100;a=1'b1;b=1'b1;
+		// Add stimulus here
+
+	end
+      
+endmodule
+
