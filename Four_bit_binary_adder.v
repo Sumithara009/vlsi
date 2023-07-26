1a5b3bdfbbbe952d7155bae6354b50ca79dd1daa
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-26 01:12:47 -0700

    Update Four_bit_binary_adder.v

--- Four_bit_binary_adder.v
+++ Four_bit_binary_adder.v
@@ -1,4 +1,4 @@
-Program: //20L152-SUMITHA R A
+//20L152-SUMITHA R A
 
 module binary4(
     input [3:0] a,b,
@@ -23,49 +23,5 @@ and g2(x[1],a,b);
 xor g3(s,x[0],x[1]);
 and g4(x[2],x[0],x[1]);
 or g5(co,x[2],x[1]);
-endmodule
-
-Test bench:
-
-module binary4test;
-
-	// Inputs
-	reg [3:0] a;
-	reg [3:0] b;
-	reg cin;
-
-	// Outputs
-	wire co;
-	wire [3:0] s;
-
-	// Instantiate the Unit Under Test (UUT)
-	binary4 uut (
-		.a(a), 
-		.b(b), 
-		.cin(cin), 
-		.co(co), 
-		.s(s)
-	);
-
-	initial begin
-		// Initialize Inputs
-		a = 0;
-		b = 0;
-		cin = 0;
-
-		// Wait 100 ns for global reset to finish
-      #100 a=4'b0001;b=4'b0011;cin=1'b0;
-      #100 a=4'b0101;b=4'b1011;cin=1'b0;
-      #100 a=4'b1101;b=4'b0011;cin=1'b1;
-      #100 a=4'b0011;b=4'b0011;cin=1'b0;
-      #100 a=4'b1001;b=4'b0011;cin=1'b1;
-      #100 a=4'b0001;b=4'b1011;cin=1'b0;
-      #100 a=4'b1111;b=4'b1101;cin=1'b1;
-  
-        
-		// Add stimulus here
-
-	end
-      
 endmodule
 
