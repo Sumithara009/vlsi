e25360d2f200254672ae137fa03c7c4b619b798a
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-29 00:41:04 -0700

    Create moore_example3_test.v

---
+++ moore_example3_test.v
@@ -0,0 +1,40 @@
+//20L152-SUMITHA R A
+module moore_thi_eg_test;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg in;
+
+	// Outputs
+	wire out;
+
+	// Instantiate the Unit Under Test (UUT)
+	moore_third_eg uut (
+		.clk(clk), 
+		.rst(rst), 
+		.in(in), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		in = 0;
+
+		// Wait 100 ns for global reset to finish
+		 #30;rst = 0;
+       #40;in = 1;
+       #10;rst = 1;in= 0;
+       #10;in = 1; 
+       #20;in = 0; 
+       #20;rst=0;in = 1; 
+       #20;in = 0; 
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
