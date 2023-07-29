fa3e96487099931c2e29687351cfe6cb25cac1a8
Sumithara009 <126504696+Sumithara009@users.noreply.github.com>
2023-07-28 23:01:08 -0700

    Update sequence_detector_1011_test.v

--- sequence_detector_1011_test.v
+++ sequence_detector_1011_test.v
@@ -16,18 +16,25 @@ module seq_det_1011_test;
 		.sequence(sequence), 
 		.tick(tick)
 	);
-   always #100 clk=~clk;
+   always#100 clk=~clk;
 	initial begin
 		// Initialize Inputs
 		clk = 0;
-		rst = 0;
+		rst = 1;
 		sequence = 0;
 
 		// Wait 100 ns for global reset to finish
-		#100 rst=1'b0;sequence=1'b0;
-		#100 rst=1'b0;sequence=1'b1;
-		#100 rst=1'b1;sequence=1'b0;
-		#100 rst=1'b1;sequence=1'b1;
+		   /*#100 rst=1'b0;sequence=1'b0;
+			#100 rst=1'b0;sequence=1'b1;
+			#100 rst=1'b1;sequence=1'b0;
+			#100 rst=1'b1;sequence=1'b1;*/
+		 #30;rst = 0;
+       #40;sequence = 1;
+       #10;sequence= 0;
+       #10;sequence= 1; 
+       #20;sequence = 0; 
+       #20;sequence = 1; 
+       #20;sequence = 0; 
         
 		// Add stimulus here
 
